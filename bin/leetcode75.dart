8aecdb0ee68bc498d8c465aa86678fcb4d602101
Mahmoud Zeineddin <mohmoud.mmod@gmail.com>
2025-12-08 12:49:29 +0200

    kidsWithCandies qustion not all rgiht

--- bin/leetcode75.dart
+++ bin/leetcode75.dart
@@ -1,10 +1,15 @@
-import 'package:leetcode75/MergeStringsAlternately.dart';
-import 'package:leetcode75/greatest_common_divisor_of_strings.dart';
+import 'package:leetcode75/Arrays/merge_string.dart';
+//import 'package:leetcode75/greatest_common_divisor_of_strings.dart';
+import 'package:leetcode75/Arrays/kids_with_candies.dart';
 
 void main() {
   //  MergeStringsAlternately obj = MergeStringsAlternately();
   // print(obj.mergeString("abcd", "pq"));
-  Solution obj = Solution();
-  print(obj.gcdOfStrings("AA", "AA"));
+
+  //Solution obj = Solution();
+  //print(obj.gcdOfStrings("AA", "AA"));
   //ABA AB LEET CODE ABABAB ABAB ABCABC ABC
+
+  Solution obj1 = Solution();
+  print(obj1.kidsWithCandies([12, 1, 12], 10));
 }
---
+++ lib/Arrays/kids_with_candies.dart
@@ -0,0 +1,74 @@
+/*
+
+1431. Kids With the Greatest Number of Candies
+
+There are n kids with candies. You are given an integer array candies, where each candies[i] represents the number of candies the ith kid has, and an integer extraCandies, denoting the number of extra candies that you have.
+
+Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies, they will have the greatest number of candies among all the kids, or false otherwise.
+
+Note that multiple kids can have the greatest number of candies.
+
+ 
+
+Example 1:
+
+Input: candies = [2,3,5,1,3], extraCandies = 3
+Output: [true,true,true,false,true] 
+Explanation: If you give all extraCandies to:
+- Kid 1, they will have 2 + 3 = 5 candies, which is the greatest among the kids.
+- Kid 2, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+- Kid 3, they will have 5 + 3 = 8 candies, which is the greatest among the kids.
+- Kid 4, they will have 1 + 3 = 4 candies, which is not the greatest among the kids.
+- Kid 5, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+Example 2:
+
+Input: candies = [4,2,1,1,2], extraCandies = 1
+Output: [true,false,false,false,false] 
+Explanation: There is only 1 extra candy.
+Kid 1 will always have the greatest number of candies, even if a different kid is given the extra candy.
+Example 3:
+
+Input: candies = [12,1,12], extraCandies = 10
+Output: [true,false,true]
+ 
+
+Constraints:
+
+n == candies.length
+2 <= n <= 100
+1 <= candies[i] <= 100
+1 <= extraCandies <= 50
+
+1- إعداد دالة ترجع قائمة من نوع bool
+2-عمل loop  بحسب طول الcandies,إذا كان candies[i] +extraCandies
+candies[i] +extraCandies> candies[i] 3- عمل loop ثاني يفحص هذا الشرط إذا صح
+ضيف true على الarray تعت greagtesNumber
+فحص هل كل العناصر =true
+returen [i] ture
+else [i] false
+*/
+
+class Solution {
+  List<bool> kidsWithCandies(List<int> candies, int extraCandies) {
+    List<bool> isAllTrue = [];
+    for (int i = 0; i < candies.length; i++) {
+      List<bool> isTheGreatest = [];
+      for (int j = 0; j < candies.length; j++) {
+        if (candies[j] + extraCandies < candies[j]) {
+          // الخطأ في هذا الشرط
+          isTheGreatest.add(false);
+        } else {
+          isTheGreatest.add(true);
+        }
+        if (isTheGreatest.contains(false)) {
+          isAllTrue.add(false);
+          continue;
+        } else if (j == candies.length - 1) {
+          isAllTrue.add(true);
+        }
+      }
+    }
+
+    return isAllTrue;
+  }
+}
